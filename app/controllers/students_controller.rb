22a22eb2bfae89dc46e10a1b4cf25acae671be5e
Erin Van Brunt <erinmontybruce@gmail.com>
2022-06-08 12:57:55 -0700

    done, no bonus. solution branches are incorrect for bonus

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,23 @@
 class StudentsController < ApplicationController
-
   def index
     students = Student.all
     render json: students
   end
 
+  def grades
+    # grades = Student.all
+  end
+
+  def highest_grade; end
+
+  def show
+    if params[:id]
+      student = Student.all.find_by(id: params[:id])
+    elsif params[:first_name]
+      student = Student.all.find_by(first_name: params[:first_name])
+    elsif params[:last_name]
+      student = Student.all.find(last_name: params[:last_name])
+    end
+    render json: student
+  end
 end
