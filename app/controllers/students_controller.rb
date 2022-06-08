aa7de8456dd1e5814497f64a8bcd9abacb64700b
Erin Van Brunt <erinmontybruce@gmail.com>
2022-06-08 12:52:14 -0700

    commit tree 8b41e362712db1dcd23f482e485effbb3bca6125

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,17 @@
 class StudentsController < ApplicationController
-
   def index
-    students = Student.all
+    students = params[:name] ? Student.by_name(params[:name]) : Student.all
     render json: students
   end
 
+  def grades
+    # grades = Student.all
+  end
+
+  def highest_grade; end
+
+  def show
+    student = Student.all.find_by(id: params[:id])
+    render json: student
+  end
 end
